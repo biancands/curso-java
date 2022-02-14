00efc25631a28b287ead0c33a7a01a0b791d5b53
Bianca Neves <biancaneves.ds@gmail.com>
2022-02-14 08:50:06 -0300

    adiciona try-catch

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -3,7 +3,13 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+        	metodo1();
+        } catch(ArithmeticException | NullPointerException ex) {
+        	String msg = ex.getMessage();
+        	System.out.println("Exception"+msg);
+        	ex.printStackTrace();
+        }
         System.out.println("Fim do main");
     }
 
@@ -17,6 +23,13 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            Conta c = null;
+            c.deposita();
+//            try {
+//            	int a = 1/0;
+//            } catch (ArithmeticException ex) {
+//            	System.out.println("ArithmeticException");
+//            }
         }
         System.out.println("Fim do metodo2");        
     }
