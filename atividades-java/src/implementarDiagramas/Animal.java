24dba7dede6effe9703a85e328687e5618adb9dd
Bianca Neves <biancaneves.ds@gmail.com>
2022-02-16 09:38:20 -0300

    modicia class animal

--- atividades-java/src/implementarDiagramas/Animal.java
+++ atividades-java/src/implementarDiagramas/Animal.java
@@ -1,5 +1,34 @@
 package implementarDiagramas;
 
-public class Animal {
+public abstract class Animal {
+	
+	private String nome;
+	private String raca;
+	private String caminha;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public void setCaminha(String caminha) {
+		this.caminha = caminha;
+	}
+	
+	public String getCaminha() {
+		return caminha;
+	}
+	
 
 }
