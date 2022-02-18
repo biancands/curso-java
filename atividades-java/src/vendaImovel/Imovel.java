d3a874980c2605109f954f3ad9f36f97413b1106
Bianca Neves <biancaneves.ds@gmail.com>
2022-02-18 10:23:29 -0300

    atualiza a classe imovel

--- atividades-java/src/vendaImovel/Imovel.java
+++ atividades-java/src/vendaImovel/Imovel.java
@@ -1,9 +1,23 @@
 package vendaImovel;
 
+import java.util.Scanner;
+
 public abstract class Imovel {
 	
 	private String endereco;
 	private double preco;
+	
+	public double vendaImoveis() {
+		
+		Scanner x = new Scanner (System.in);
+		System.out.println("Digite a opção do valor desejado:\n "+"A -> imovel novo\n "+"B -> imovel velho");
+		String opcao = x.nextLine();
+		
+		if (opcao == "A") {
+			
+		}
+		
+	}
 	
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
