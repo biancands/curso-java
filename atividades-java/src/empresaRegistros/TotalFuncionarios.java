0e6f481f720459099403a2bf61a9847ea4fc30b3
Bianca Neves <biancaneves.ds@gmail.com>
2022-02-22 09:18:58 -0300

    atualiza novo pacote

--- atividades-java/src/empresaRegistros/TotalFuncionarios.java
+++ atividades-java/src/empresaRegistros/TotalFuncionarios.java
@@ -1,5 +1,22 @@
 package empresaRegistros;
 
 public class TotalFuncionarios {
+	
+	public static void main(String[] args) {
+		
+		Funcionario f[] = new Funcionario[10];
+		f[0] = new FuncionarioEnsinoBasico();
+		f[1] = new FuncionarioEnsinoBasico();
+		f[2] = new FuncionarioEnsinoBasico();
+		f[3] = new FuncionarioEnsinoBasico();
+		f[4] = new FuncionarioEnsinoMedio();
+		f[5] = new FuncionarioEnsinoMedio();
+		f[6] = new FuncionarioEnsinoMedio();
+		f[7] = new FuncionarioEnsinoMedio();
+		f[8] = new FuncionarioGraduado();
+		f[9] = new FuncionarioGraduado();
+		
+		
+	}
 
 }
